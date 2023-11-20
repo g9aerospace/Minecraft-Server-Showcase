509a8d4a748973d59b39ac7da03dad71e719ed53
G9 Aerospace <80208426+g9militantsYT@users.noreply.github.com>
2023-11-21 03:14:31 +0530

    even sends the webhooks and works much better! wow

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { Client, Intents } = require('discord.js');
 const dotenv = require('dotenv');
 const dns = require('dns');
 const winston = require('winston');
+const axios = require('axios');
 
 // Load environment variables from .env file
 dotenv.config();
@@ -11,7 +12,6 @@ const client = new Client({
   intents: [
     Intents.FLAGS.GUILDS,         // Enable guild-related events
     Intents.FLAGS.GUILD_MESSAGES, // Enable message-related events
-    // Add other intents your bot requires
   ],
 });
 
@@ -25,6 +25,10 @@ const logger = winston.createLogger({
   ],
 });
 
+// Webhook URLs for summaries and invalid input
+const webhookUrlSummary = process.env.WEBHOOK_URL_SUMMARY;
+const webhookUrlInvalidInput = process.env.WEBHOOK_URL_INVALID_INPUT;
+
 // Event listener when the bot is ready
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
@@ -35,76 +39,97 @@ client.on('messageCreate', async (message) => {
   // Ignore messages from bots
   if (message.author.bot) return;
 
-  // Check if the message is in the specified channel
-  const channelId = process.env.CHANNEL_ID;
-  if (message.channel.id !== channelId) return;
+  // Extract all domain and port pairs from the user's message
+  const userDomainAndPorts = extractAllDomainsAndPorts(message.content);
 
-  // Get the content of the user's message
-  const userMessageContent = message.content.trim();
-
-  // Extract domain and port from the user's message
-  const userDomainAndPort = extractDomainAndPort(userMessageContent);
-
-  if (!userDomainAndPort) {
+  if (!userDomainAndPorts.length) {
     // Invalid user input
-    logError('Invalid user input', userMessageContent);
+    const errorMessage = `Invalid user input: ${message.content}`;
+    logError(errorMessage);
+    sendSummary(webhookUrlInvalidInput, errorMessage);
     // Ask the user to provide a valid domain and port
     message.reply('Please provide a valid domain and port in the format: `domain:port`');
     return;
   }
 
-  // Check if domain or port is missing and ask for the missing information
-  const { domain, port } = userDomainAndPort;
-  if (!domain) {
-    message.reply('Please provide a valid domain in the format: `domain:port`');
-    return;
-  }
+  // Process each domain:port pair in the message separately
+  for (const userDomainAndPort of userDomainAndPorts) {
+    const { domain, port } = userDomainAndPort;
 
-  if (!port) {
-    message.reply('Please provide a valid port in the format: `domain:port`');
-    return;
-  }
+    // Check if domain or port is missing and ask for the missing information
+    if (!domain) {
+      const errorMessage = 'Invalid user input: Missing domain';
+      logError(errorMessage);
+      sendSummary(webhookUrlInvalidInput, errorMessage);
+      message.reply('Please provide a valid domain in the format: `domain:port`');
+      return;
+    }
 
-  try {
-    // Resolve IP addresses for pre-specified domains
-    const preSpecifiedDomains = ['hel1.bbn.one', 'sgp1.bbn.one', 'mum1.bbn.one', 'fsn1.bbn.one'];
-    const domainToIPMap = await resolveIPs(preSpecifiedDomains);
+    if (!port) {
+      const errorMessage = 'Invalid user input: Missing port';
+      logError(errorMessage);
+      sendSummary(webhookUrlInvalidInput, errorMessage);
+      message.reply('Please provide a valid port in the format: `domain:port`');
+      return;
+    }
 
-    // Resolve IP address for the user-provided domain
-    const userDomain = userDomainAndPort.domain;
-    const userDomainIP = await resolveIP(userDomain);
+    try {
+      // Record the start time
+      const startTime = new Date();
 
-    // Check if the user-provided IP matches any of the pre-specified domains
-    const isValidIP = preSpecifiedDomains.some((domain) => domainToIPMap[domain] === userDomainIP);
+      // Resolve IP address for the user-provided domain
+      const userDomainIP = await resolveIP(domain);
 
-    // React to the message based on the result
-    if (isValidIP) {
-      // Scenario A: User-provided IP matches a pre-specified domain
-      message.react('👍');
-    } else {
-      // Scenario B: User-provided IP does not match any pre-specified domain
+      // Record the end time
+      const endTime = new Date();
+
+      // Calculate the processing time
+      const processingTime = endTime - startTime;
+
+      // Resolve IP addresses for pre-specified domains
+      const preSpecifiedDomains = ['hel1.bbn.one', 'sgp1.bbn.one', 'mum1.bbn.one', 'fsn1.bbn.one'];
+      const domainToIPMap = await resolveIPs(preSpecifiedDomains);
+
+      // Check if the user-provided IP matches any of the pre-specified domains
+      const isValidIP = preSpecifiedDomains.some((domain) => domainToIPMap[domain] === userDomainIP);
+
+      // React to the message based on the result
+      if (isValidIP) {
+        // Scenario A: User-provided IP matches a pre-specified domain
+        message.react('👍');
+        const summary = `Processed message successfully. User provided domain: ${domain}, Valid IP: ${userDomainIP}, Processing Time: ${processingTime}ms`;
+        sendSummary(webhookUrlSummary, summary);
+      } else {
+        // Scenario B: User-provided IP does not match any pre-specified domain
+        message.react('❌');
+        const errorMessage = `Processed message with invalid domain. User provided domain: ${domain}, Invalid IP: ${userDomainIP}, Processing Time: ${processingTime}ms`;
+        logError(errorMessage);
+        sendSummary(webhookUrlInvalidInput, errorMessage);
+      }
+    } catch (error) {
+      // Handle errors
+      const errorMessage = `Error processing message: ${error.message}`;
+      logError(errorMessage);
+      sendSummary(webhookUrlInvalidInput, errorMessage);
       message.react('❌');
     }
-  } catch (error) {
-    // Handle errors
-    logError('Error processing message', error.message);
-    message.react('❌');
   }
 });
 
-// Function to extract domain and port from the user's message
-function extractDomainAndPort(message) {
-  const match = message.match(/^(.+?):(\d+)$/);
-  if (match) {
-    const domain = match[1];
-    const port = match[2];
-
-    // Validate domain (you might want to use a more robust validation method)
-    if (isValidDomain(domain)) {
-      return { domain, port };
-    }
+// Function to extract all domain and port pairs from the user's message
+function extractAllDomainsAndPorts(message) {
+  const matches = message.match(/(?:^|\s)(.+?):(\d+)(?=\s|$)/g);
+  if (matches) {
+    return matches.map(match => {
+      const [_, domain, port] = match.match(/(.+?):(\d+)/);
+      // Validate domain (you might want to use a more robust validation method)
+      if (isValidDomain(domain)) {
+        return { domain, port };
+      }
+      return null;
+    }).filter(Boolean); // Filter out null entries
   }
-  return null;
+  return [];
 }
 
 // Function to validate a domain (you can customize this validation)
@@ -125,7 +150,7 @@ async function resolveIPs(domains) {
 // Function to resolve the IP address for a domain
 function resolveIP(domain) {
   return new Promise((resolve, reject) => {
-    dns.resolve(domain, (error, addresses) => {
+    dns.resolve(domain, 'A', (error, addresses) => {
       if (error) {
         reject(error);
       } else {
@@ -135,9 +160,18 @@ function resolveIP(domain) {
   });
 }
 
+// Function to send a detailed summary to a webhook
+async function sendSummary(webhookUrl, summary) {
+  try {
+    await axios.post(webhookUrl, { content: summary });
+  } catch (error) {
+    logger.error(`Failed to send summary to webhook (${webhookUrl}): ${error.message}`);
+  }
+}
+
 // Function to log errors with details
-function logError(message, details) {
-  logger.error(`${message}: ${details}`);
+function logError(message) {
+  logger.error(message);
 }
 
 // Login to Discord with the bot token
