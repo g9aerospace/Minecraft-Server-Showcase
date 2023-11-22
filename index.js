efc857adb7783c02916ac2d856659d8c80d01682
G9 Aerospace <80208426+g9militantsYT@users.noreply.github.com>
2023-11-23 02:01:50 +0530

    gg

--- index.js
+++ index.js
@@ -21,166 +21,178 @@ const monitoredChannelId = process.env.CHANNEL_ID;
 const webhookUrlInvalidInput = process.env.WEBHOOK_URL_INVALID_INPUT;
 const webhookUrlError = process.env.WEBHOOK_URL_ERROR;
 
+// Add a Map to keep track of users and their last interaction times
+const userTimeouts = new Map();
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on('messageCreate', async (message) => {
-  if (message.author.bot || message.channel.id !== monitoredChannelId) {
-    // Ignore messages from bots and channels other than the specified one
-    return;
-  }
-
-  const userDomainAndPorts = extractAllDomainsAndPorts(message.content);
-
-  if (!userDomainAndPorts.length) {
-    const errorMessage = `Invalid user input: ${message.content}`;
-    logError(errorMessage, webhookUrlInvalidInput);
-    const reply = await message.reply('Please provide a valid domain and port in the format: `domain:port`');
-
-    // Automatically delete only the user's message after 10 seconds
-    deleteMessagesAfterDelay([message, reply], 10000);
-    return;
-  }
-
-  for (const userDomainAndPort of userDomainAndPorts) {
-    const { domain, port } = userDomainAndPort;
-
-    if (!domain) {
-      const errorMessage = 'Invalid user input: Missing domain';
-      logError(errorMessage, webhookUrlInvalidInput);
-      const reply = await message.reply('Please provide a valid domain in the format: `domain:port`');
-
-      // Automatically delete only the user's message after 10 seconds
-      deleteMessagesAfterDelay([message, reply], 10000);
+  try {
+    if (message.author.bot || message.channel.id !== monitoredChannelId) {
+      // Ignore messages from bots and channels other than the specified one
       return;
     }
 
-    if (!port) {
-      const errorMessage = 'Invalid user input: Missing port';
+    console.log(`Received message from user ${message.author.tag}: "${message.content}"`);
+
+    const userDomainAndPorts = extractAllDomainsAndPorts(message.content);
+
+    if (!userDomainAndPorts.length) {
+      const errorMessage = `Invalid user input: ${message.content}`;
       logError(errorMessage, webhookUrlInvalidInput);
-      const reply = await message.reply('Please provide a valid port in the format: `domain:port`');
+      const reply = await message.reply('Please provide a valid domain and port in the format: `domain:port`');
 
       // Automatically delete only the user's message after 10 seconds
-      deleteMessagesAfterDelay([message, reply], 10000);
+      await deleteMessagesWithTimeout([message, reply], 10000);
       return;
     }
 
-    try {
-      const startTime = new Date();
-      const userDomainIP = await resolveIP(domain);
-      const endTime = new Date();
-      const processingTime = endTime - startTime;
+    for (const userDomainAndPort of userDomainAndPorts) {
+      const { domain, port } = userDomainAndPort;
 
-      const preSpecifiedDomains = ['2.223.144.35'];
-      const domainToIPMap = await resolveIPs(preSpecifiedDomains);
-      const isValidIP = preSpecifiedDomains.some((domain) => domainToIPMap[domain] === userDomainIP);
+      if (!domain) {
+        const errorMessage = 'Invalid user input: Missing domain';
+        logError(errorMessage, webhookUrlInvalidInput);
+        const reply = await message.reply('Please provide a valid domain in the format: `domain:port`');
 
-      console.log(`isValidIP: ${isValidIP}`);
+        // Automatically delete only the user's message after 10 seconds
+        await deleteMessagesWithTimeout([message, reply], 10000);
+        return;
+      }
 
-      if (isValidIP) {
-        message.react('👍');
+      if (!port) {
+        const errorMessage = 'Invalid user input: Missing port';
+        logError(errorMessage, webhookUrlInvalidInput);
+        const reply = await message.reply('Please provide a valid port in the format: `domain:port`');
 
-        const parsedPort = parseInt(port);
+        // Automatically delete only the user's message after 10 seconds
+        await deleteMessagesWithTimeout([message, reply], 10000);
+        return;
+      }
 
-        console.log(`Parsed port: ${parsedPort}`);
-        console.log(`Parsed port type: ${typeof parsedPort}`);
-        console.log(`Parsed port value: ${parsedPort}`);
-        console.log(`Condition: ${parsedPort === 25565}`);
+      try {
+        const startTime = new Date();
+        const userDomainIP = await resolveIP(domain);
+        const endTime = new Date();
+        const processingTime = endTime - startTime;
 
-        if (!isNaN(parsedPort)) {
-          console.log('Inside block for Minecraft server port');
-          try {
-            const serverDetails = await queryMinecraftServer(domain, parsedPort);
-            console.log('After querying Minecraft server');
+        const preSpecifiedDomains = ['2.223.144.35'];
+        const domainToIPMap = await resolveIPs(preSpecifiedDomains);
+        const isValidIP = preSpecifiedDomains.some((domain) => domainToIPMap[domain] === userDomainIP);
 
-            const motd = serverDetails.description.text;
-            const playersOnline = serverDetails.players ? serverDetails.players.online : 'N/A';
+        console.log(`isValidIP: ${isValidIP}`);
 
-            console.log(`MOTD: ${motd}`);
-            console.log(`Players Online: ${playersOnline}`);
+        if (isValidIP) {
+          message.react('👍');
 
-            // Load the background image
-            const backgroundImage = await loadImage('minecraft_background.png');
+          const parsedPort = parseInt(port);
 
-            // Create a canvas and set its dimensions
-            const canvas = createCanvas(400, 100);
-            const context = canvas.getContext('2d');
+          console.log(`Parsed port: ${parsedPort}`);
+          console.log(`Parsed port type: ${typeof parsedPort}`);
+          console.log(`Parsed port value: ${parsedPort}`);
+          console.log(`Condition: ${parsedPort === 25565}`);
 
-            // Draw the background image on the canvas
-            context.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+          if (!isNaN(parsedPort)) {
+            console.log('Inside block for Minecraft server port');
+            try {
+              const serverDetails = await queryMinecraftServer(domain, parsedPort);
+              console.log('After querying Minecraft server');
 
-            // Set the font for MOTD
-            context.font = '20px Minecraft';
+              const motd = serverDetails.description.text;
+              const playersOnline = serverDetails.players ? serverDetails.players.online : 'N/A';
 
-            // Set the text color
-            context.fillStyle = '#ffffff'; // Set it to the color used in Minecraft MOTD
+              console.log(`MOTD: ${motd}`);
+              console.log(`Players Online: ${playersOnline}`);
 
-            // Draw the MOTD text on the canvas
-            context.fillText(motd, 10, 30);
+              // Load the background image
+              const backgroundImage = await loadImage('minecraft_background.png');
 
-            // Convert the canvas to a buffer
-            const buffer = canvas.toBuffer();
+              // Create a canvas and set its dimensions
+              const canvas = createCanvas(400, 100);
+              const context = canvas.getContext('2d');
 
-            // Create an embed for sending the MOTD as an image attachment
-            const embed = {
-              title: `${domain}:${parsedPort}`,
-              description: `${message.content}`,
-              fields: [
-                {
-                  name: 'Players Online',
-                  value: playersOnline.toString(), // Convert to string to ensure it's a valid field value
-                  inline: true, // You can adjust this based on your embed design
+              // Draw the background image on the canvas
+              context.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+
+              // Set the font for MOTD
+              context.font = '20px Minecraft';
+
+              // Set the text color
+              context.fillStyle = '#ffffff'; // Set it to the color used in Minecraft MOTD
+
+              // Draw the MOTD text on the canvas
+              context.fillText(motd, 10, 30);
+
+              // Convert the canvas to a buffer
+              const buffer = canvas.toBuffer();
+
+              // Create an embed for sending the MOTD as an image attachment
+              const embed = {
+                title: `${domain}:${parsedPort}`,
+                description: `${message.content}`,
+                fields: [
+                  {
+                    name: 'Players Online',
+                    value: playersOnline.toString(), // Convert to string to ensure it's a valid field value
+                    inline: true, // You can adjust this based on your embed design
+                  },
+                ],
+                footer: {
+                  text: message.author.username,
                 },
-              ],
-              footer: {
-                text: message.author.username,
-              },
-              image: {
-                url: `attachment://motd.png`,
-              },
-            };
+                image: {
+                  url: `attachment://motd.png`,
+                },
+              };
 
-            // Send the embed with the image attachment
-            const attachment = new MessageAttachment(buffer, 'motd.png');
-            const sentMessage = await message.channel.send({ embeds: [embed], files: [attachment] });
+              // Send the embed with the image attachment
+              const attachment = new MessageAttachment(buffer, 'motd.png');
+              const sentMessage = await message.channel.send({ embeds: [embed], files: [attachment] });
 
-            // Delete only the user's message after 10 seconds
-            deleteMessagesAfterDelay([message, sentMessage], 10000);
+              // After successfully sending the embed, timeout the user for 24 hours
+              await deleteMessagesWithTimeout([message, sentMessage], 24 * 60 * 60 * 1000); // 24 hours timeout
+              userTimeouts.set(message.author.id, Date.now() + 24 * 60 * 60 * 1000); // Set timeout for the user
 
-          } catch (error) {
-            console.log(`Error querying Minecraft server: ${error.message}`);
-            logError(`Error querying Minecraft server: ${error.message}`, webhookUrlError);
-            message.react('❗');
-            return;
+            } catch (error) {
+              console.log(`Error querying Minecraft server: ${error.message}`);
+              logError(`Error querying Minecraft server: ${error.message}`, webhookUrlError);
+              message.react('❗');
+              return;
+            }
+          } else {
+            console.log('Parsed port is not a valid integer. Skipping Minecraft server details.');
           }
         } else {
-          console.log('Parsed port is not a valid integer. Skipping Minecraft server details.');
+          message.react('⚠');
+          const errorMessage = `Processed message with an invalid domain. User provided domain: ${domain}, Invalid IP: ${userDomainIP}, Processing Time: ${processingTime}ms`;
+          logError(errorMessage, webhookUrlInvalidInput);
         }
-      } else {
-        message.react('⚠');
-        const errorMessage = `Processed message with an invalid domain. User provided domain: ${domain}, Invalid IP: ${userDomainIP}, Processing Time: ${processingTime}ms`;
-        logError(errorMessage, webhookUrlInvalidInput);
+      } catch (error) {
+        const errorMessage = `Error processing message: ${error.message}`;
+        logError(errorMessage, webhookUrlError);
+        message.react('👀');
       }
-    } catch (error) {
-      const errorMessage = `Error processing message: ${error.message}`;
-      logError(errorMessage, webhookUrlError);
-      message.react('👀');
     }
+  } catch (error) {
+    console.error(`Error processing message event: ${error.message}`);
+    logError(`Error processing message event: ${error.message}`, webhookUrlError);
   }
 });
 
-// Add a function to delete only the user's message after a specified delay
-async function deleteMessagesAfterDelay(messages, delay) {
-  // Wait for the specified delay
-  await setTimeout(delay);
-
+// Add a function to delete only the user's message after a specified delay, with extended timeout
+async function deleteMessagesWithTimeout(messages, delay) {
   try {
+    // Wait for the specified delay
+    await setTimeout(delay);
+
     // Delete only the user's message in the array
     for (const msg of messages) {
       if (!msg.author.bot) {
         // Delete only if the message is not from a bot (user's message)
         await msg.delete();
+        console.log(`Deleted message from user ${msg.author.tag}: "${msg.content}"`);
       }
     }
   } catch (error) {
