b315df9a989820a7325a82bb84338bdfe15f8c5a
G9 Aerospace <80208426+g9militantsYT@users.noreply.github.com>
2023-11-24 02:09:03 +0530

    fixes!

--- mc.js
+++ mc.js
@@ -85,7 +85,9 @@ mcBot.once('ready', async () => {
 
         try {
           // Send an embed with pre-existing data
-          const existingUserData = JSON.parse(fs.readFileSync(userDataFilePath, 'utf-8'));
+          const existingUserData = readUserData(userDataFilePath, user);
+          if (!existingUserData) return;
+
           const embedChannel = await mcBot.channels.fetch(process.env.CHANNEL_ID);
 
           const embed = new MessageEmbed()
@@ -114,11 +116,12 @@ mcBot.once('ready', async () => {
           const userDataFilePath = `userdata/${user.id}.json`;
 
           try {
-            const existingUserData = JSON.parse(fs.readFileSync(userDataFilePath, 'utf-8'));
+            const existingUserData = readUserData(userDataFilePath, user);
+            if (!existingUserData) return;
 
             // Inform the user about their existing data and provide an option to edit
             await user.send(`Existing Server Address: ${existingUserData.address || 'Not provided'}`);
-            await user.send('If you want to edit the server address, click the "Edit" button below.');
+            await user.send('If you want to edit the server address or invite message, click the "Edit" button below.');
 
             // Provide an edit button
             const editButtonMessage = await user.send({
@@ -137,44 +140,43 @@ mcBot.once('ready', async () => {
               ],
             });
 
-        // Event listener for the edit button click
-        const filterEditButton = (buttonInteraction) => buttonInteraction.customId === 'edit_info' && buttonInteraction.user.id === user.id;
-        const editButtonInteraction = await editButtonMessage.awaitMessageComponent({ filter: filterEditButton, time: 60000 });
+            // Event listener for the edit button click
+            const filterEditButton = (buttonInteraction) => buttonInteraction.customId === 'edit_info' && buttonInteraction.user.id === user.id;
+            const editButtonInteraction = await editButtonMessage.awaitMessageComponent({ filter: filterEditButton, time: 60000 });
 
-        // Check if the user clicked the edit button
-        if (editButtonInteraction) {
-        // Allow the user to edit their previous message
-        await user.send('Please provide the updated address of your Minecraft server in the format domain:port or ip:port, whichever suits you.');
+            // Check if the user clicked the edit button
+            if (editButtonInteraction) {
+              // Allow the user to edit their previous message
+              await user.send('Please provide the updated address of your Minecraft server in the format domain:port or ip:port, whichever suits you.');
 
-        const filterEditAddress = (msg) => msg.author.id === user.id;
-        const responseEditAddress = await user.dmChannel.awaitMessages({ filter: filterEditAddress, max: 1, time: 60000, errors: ['time'] });
+              const filterEditAddress = (msg) => msg.author.id === user.id;
+              const responseEditAddress = await user.dmChannel.awaitMessages({ filter: filterEditAddress, max: 1, time: 60000, errors: ['time'] });
 
-        const updatedAddress = responseEditAddress.first().content;
+              const updatedAddress = responseEditAddress.first().content;
 
-        // Ask for an updated invite message
-        await user.send('Please provide the updated invite message for your Minecraft server.');
+              // Ask for an updated invite message
+              await user.send('Please provide the updated Invite Message without any IP addresses.');
 
-        const filterEditInviteMessage = (msg) => msg.author.id === user.id;
-        const responseEditInviteMessage = await user.dmChannel.awaitMessages({
-            filter: filterEditInviteMessage,
-            max: 1,
-            time: 60000,
-            errors: ['time'],
-        });
+              const filterEditInviteMessage = (msg) => msg.author.id === user.id;
+              const responseEditInviteMessage = await user.dmChannel.awaitMessages({
+                filter: filterEditInviteMessage,
+                max: 1,
+                time: 60000,
+                errors: ['time'],
+              });
 
-        const updatedInviteMessage = responseEditInviteMessage.first().content;
+              const updatedInviteMessage = responseEditInviteMessage.first().content;
 
-        // Update the existing user data
-        existingUserData.address = updatedAddress;
-        existingUserData.inviteMessage = updatedInviteMessage;
-        fs.writeFileSync(userDataFilePath, JSON.stringify(existingUserData));
+              // Update the existing user data
+              existingUserData.address = updatedAddress;
+              existingUserData.inviteMessage = updatedInviteMessage;
+              fs.writeFileSync(userDataFilePath, JSON.stringify(existingUserData));
 
-        await user.send('Your server address and invite message have been updated. Thank you!');
-        } else {
-        // Handle the case where the user didn't click the edit button
-        await user.send('You chose not to edit your previous message. If you have any other questions, feel free to ask.');
-        }
-
+              await user.send('Your server address and invite message have been updated. Thank you!');
+            } else {
+              // Handle the case where the user didn't click the edit button
+              await user.send('You chose not to edit your previous message. If you have any other questions, feel free to ask.');
+            }
           } catch (error) {
             console.error('Error reading existing user data:', error);
             await user.send('An error occurred while trying to retrieve your existing data. Please try again later.');
@@ -254,3 +256,24 @@ mcBot.once('ready', async () => {
 mcBot.on('error', (error) => {
   fs.appendFileSync('error.log', `[${new Date()}] ${error}\n`);
 });
+
+function readUserData(filePath, user) {
+  try {
+    const existingUserData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    return existingUserData;
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.warn(`User data file does not exist for user ${user.id}. Creating a new file.`);
+      const defaultUserData = {
+        userId: user.id,
+        address: 'Not provided',
+        inviteMessage: 'No invite message provided',
+      };
+      fs.writeFileSync(filePath, JSON.stringify(defaultUserData));
+      return defaultUserData;
+    }
+
+    console.error(`Error reading existing user data for user ${user.id} at path ${filePath}:`, error);
+    return null;
+  }
+}
