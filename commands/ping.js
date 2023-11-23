a37296118524a3f1ba4e6ce6b7387b3ac1a602cf
G9 Aerospace <80208426+g9militantsYT@users.noreply.github.com>
2023-11-23 21:49:43 +0530

    suiiiiiiii

---
+++ commands/ping.js
@@ -0,0 +1,11 @@
+// commands/ping.js
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Replies with Pong!'),
+  async execute(interaction) {
+    await interaction.reply('Pong!');
+  },
+};
--- index.js
+++ index.js
@@ -1,26 +1,64 @@
-// Import necessary modules and environment variables
+require('dotenv').config();
 const { Client, Intents } = require('discord.js');
-const dotenv = require('dotenv');
-
-dotenv.config();
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const fs = require('fs');
 
 const client = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
     Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.MESSAGE_CONTENT,
   ],
 });
 
-// Import other files
-const { handleCommand } = require('./commands');
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-// Event listener for 'ready'
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commands },
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  // Check if the user has the required role
+  const requiredRole = process.env.ROLE_ID;
+  if (!interaction.member.roles.cache.has(requiredRole)) {
+    return interaction.reply({
+      content: 'You do not have the required role to use this command.',
+      ephemeral: true,
+    });
+  }
+
+  try {
+    // Execute the command dynamically based on the commandName
+    const command = require(`./commands/${commandName}`);
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    return interaction.reply({ content: 'Error executing the command.', ephemeral: true });
+  }
 });
 
-// Event listener for 'messageCreate'
-client.on('messageCreate', handleCommand);
-
-// Log in to Discord
 client.login(process.env.BOT_TOKEN);
