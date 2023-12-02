3b2986a359bfd9edecd327926d51f9b3285cba91
G9 Aerospace <80208426+g9militantsYT@users.noreply.github.com>
2023-12-02 22:28:36 +0530

    works rather well!

--- commands/showcase.js
+++ commands/showcase.js
@@ -1,5 +1,9 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const fs = require('fs');
+const dns = require('dns');
+
+// Array of allowed IP addresses
+const allowedIPs = ['51.255.80.17', '46.250.234.25', '46.250.234.35', '2.223.144.35'];
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -13,26 +17,42 @@ module.exports = {
       if (fs.existsSync(filePath)) {
         const serverData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
 
-        // Assuming CHANNEL_ID is defined in your .env file
-        const channelId = process.env.CHANNEL_ID;
-        const channel = interaction.client.channels.cache.get(channelId);
+        // Resolve IP address
+        dns.resolve4(serverData.ip, (err, addresses) => {
+          if (err) {
+            console.error(`Error resolving IP address for user ${userId}: ${err}`);
+            interaction.reply('There was an error resolving the IP address. Try adding your server again with the addserver command!');
+            return;
+          }
 
-        if (channel) {
-          await channel.send(`Server Information for User ${interaction.user.tag}:
-            IP: ${serverData.ip}
-            Port: ${serverData.port}
-            Message: ${serverData.message}`);
-          await interaction.reply('Server information showcased successfully!');
-        } else {
-          console.error(`Channel with ID ${channelId} not found.`);
-          await interaction.reply('There was an error showcasing server information.');
-        }
+          const resolvedIP = addresses[0];
+
+          // Check if the resolved IP matches any allowed IP
+          if (allowedIPs.includes(resolvedIP)) {
+            // Assuming CHANNEL_ID is defined in your .env file
+            const channelId = process.env.CHANNEL_ID;
+            const channel = interaction.client.channels.cache.get(channelId);
+
+            if (channel) {
+              channel.send(`Server Information for User ${interaction.user.tag}:
+                IP: ${serverData.ip}
+                Port: ${serverData.port}
+                Message: ${serverData.message}`);
+              interaction.reply('Server information showcased successfully!');
+            } else {
+              console.error(`Channel with ID ${channelId} not found.`);
+              interaction.reply('There was an error showcasing server information.');
+            }
+          } else {
+            interaction.reply('Invalid IP address. Please provide a valid IP address.');
+          }
+        });
       } else {
-        await interaction.reply('No server information found. Use /addserver to add server information.');
+        interaction.reply('No server information found. Use /addserver to add server information.');
       }
     } catch (error) {
       console.error(`Error showcasing server information for user ${userId}: ${error}`);
-      await interaction.reply('There was an error while showcasing server information.');
+      interaction.reply('There was an error while showcasing server information.');
     }
   },
 };
