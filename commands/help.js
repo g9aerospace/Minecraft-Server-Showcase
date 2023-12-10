8c4c2cd41516a2cab6f487b8403b0214d1b6f974
G9 Aerospace <80208426+g9militantsYT@users.noreply.github.com>
2023-12-10 20:44:13 +0530

    added a help command

--- commands/help.js
+++ commands/help.js
@@ -1,12 +1,24 @@
 // commands/help.js
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('help')
     .setDescription('Displays a help message'),
   async execute(interaction) {
-    const helpMessage = 'This is a simple help message. Customize it as needed.';
-    await interaction.reply(helpMessage);
+    const embed = new MessageEmbed()
+      .setTitle('Command Help')
+      .setDescription('Here are the available commands and their descriptions:')
+      .addFields(
+        { name: '/addserver', value: 'Set the user\'s server to showcase, in the database' },
+        { name: '/showcase', value: 'Showcase the user\'s server' },
+        { name: '/userinfo', value: 'Obtain details of a user\'s server from the database' },
+        { name: '/whitelistedips', value: 'Obtain a list of currently whitelisted IPs that can be showcased' },
+      )
+      .setColor('#3498db') // Set the color to a shade of blue
+      .setFooter('Embernodes', 'attachment://embernodes.png'); // Set footer text and icon
+
+    await interaction.reply({ embeds: [embed], files: ['./embernodes.png'] });
   },
 };
