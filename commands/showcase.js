fb272bf37036826dbcc42760d955535cb6336785
G9 Aerospace <80208426+g9militantsYT@users.noreply.github.com>
2023-12-19 01:07:58 +0530

    better

--- commands/showcase.js
+++ commands/showcase.js
@@ -25,7 +25,7 @@ module.exports = {
       if (timeSinceLastExecution < cooldownDuration) {
         const remainingCooldown = cooldownDuration - timeSinceLastExecution;
         const remainingCooldownHours = Math.ceil(remainingCooldown / (60 * 60 * 1000));
-        
+
         await interaction.reply(`Command is on cooldown. Please wait ${remainingCooldownHours} hours before using it again.`);
         return;
       }
@@ -42,61 +42,83 @@ module.exports = {
       const { address, message: userMessage } = serverData;
 
       const apiUrl = `https://api.mcsrvstat.us/2/${address}`;
-      const response = await axios.get(apiUrl);
 
-      if (response.status !== 200 || !response.data.online) {
-        await interaction.reply('There was an error querying the server information. Please make sure the server is online and the address is correct.');
-        return;
-      }
+      try {
+        const response = await axios.get(apiUrl);
 
-      console.log('Minecraft Server Response:', response.data);
-
-      // Check if the Minecraft server is online
-      if (response.data.online) {
-        const whitelistedIps = fs.readFileSync('whitelisted-ips.txt', 'utf8').split('\n').map(ip => ip.trim());
-        const resolvedIp = await axios.get(`https://api.mcsrvstat.us/2/${address}`);
-        const serverRootDomain = resolvedIp.data.ip || '';
-
-        if (!whitelistedIps.includes(serverRootDomain)) {
-          await interaction.reply('The server IP does not match any whitelisted IP. Server information will not be showcased.');
-          return;
+        if (response.status !== 200 || !response.data.online) {
+          throw new Error('Error querying server information.');
         }
+
+        console.log('Minecraft Server Response:', response.data);
+
+        // Check if the Minecraft server is online
+        if (response.data.online) {
+          const whitelistedIps = fs.readFileSync('whitelisted-ips.txt', 'utf8').split('\n').map(ip => ip.trim());
+
+          try {
+            const resolvedIp = await axios.get(`https://api.mcsrvstat.us/2/${address}`);
+            const serverRootDomain = resolvedIp.data.ip || '';
+
+            if (!whitelistedIps.includes(serverRootDomain)) {
+              throw new Error('Server IP does not match any whitelisted IP.');
+            }
+          } catch (whitelistError) {
+            console.error('Whitelist Error:', whitelistError.message);
+            await interaction.reply('There was an error checking the server whitelist.');
+            return;
+          }
+        }
+
+        const user = interaction.user;
+
+        try {
+          // Generate Minecraft-style MOTD image with dirt background
+          const motdText = response.data.motd.clean.length > 0 ? response.data.motd.clean[0] : 'No MOTD available';
+          const motdImageUrl = `https://via.placeholder.com/400x20/0099FF/FFFFFF?text=${encodeURIComponent(motdText)}&bg=https://via.placeholder.com/400x20/8B4513/8B4513`;
+
+          const embed = new MessageEmbed()
+            .setTitle('Minecraft Server Showcase')
+            .setColor('#0099FF')
+            .addFields(
+              { name: 'Server IP', value: address, inline: true },
+              { name: 'Players Online', value: `${response.data.players.online}/${response.data.players.max}`, inline: true },
+              { name: 'Version', value: response.data.version, inline: true },
+              { name: 'User Message', value: userMessage || 'No custom message provided' },
+              { name: 'Software', value: response.data.software || 'Unknown', inline: true }
+            )
+            .setImage(motdImageUrl)
+            .setThumbnail(response.data.icon ? `https://api.mcsrvstat.us/icon/${address}` : 'https://via.placeholder.com/64')
+            .setFooter(user.username, user.displayAvatarURL({ dynamic: true }));
+
+          const targetChannelId = process.env.CHANNEL_ID;
+          const targetChannel = await interaction.client.channels.fetch(targetChannelId);
+
+          try {
+            const sentMessage = await targetChannel.send({ embeds: [embed] });
+
+            console.log('Message sent successfully:', sentMessage);
+
+            // Update the user's last execution time
+            updateUserLastExecutionTime(userId);
+
+            await interaction.reply(`Server information sent to <#${targetChannelId}>. [View Message](${sentMessage.url})`);
+            console.log('Interaction reply successful');
+          } catch (sendMessageError) {
+            console.error('Send Message Error:', sendMessageError.message);
+            await interaction.reply('There was an error sending the server information message.');
+          }
+        } catch (embedError) {
+          console.error('Embed Generation Error:', embedError.message);
+          await interaction.reply('There was an error generating the server information embed.');
+        }
+      } catch (apiError) {
+        console.error('API Error:', apiError.message);
+        await interaction.reply('There was an error querying the server information.');
       }
-
-      const user = interaction.user;
-
-      // Generate Minecraft-style MOTD image with dirt background
-      const motdText = response.data.motd.clean.length > 0 ? response.data.motd.clean[0] : 'No MOTD available';
-      const motdImageUrl = `https://via.placeholder.com/400x20/0099FF/FFFFFF?text=${encodeURIComponent(motdText)}&bg=https://via.placeholder.com/400x20/8B4513/8B4513`;
-
-      const embed = new MessageEmbed()
-        .setTitle('Minecraft Server Showcase')
-        .setColor('#0099FF')
-        .addFields(
-          { name: 'Server IP', value: address, inline: true },
-          { name: 'Players Online', value: `${response.data.players.online}/${response.data.players.max}`, inline: true },
-          { name: 'Version', value: response.data.version, inline: true },
-          { name: 'User Message', value: userMessage || 'No custom message provided' },
-          { name: 'Software', value: response.data.software || 'Unknown', inline: true }
-        )
-        .setImage(motdImageUrl)
-        .setThumbnail(response.data.icon ? `https://api.mcsrvstat.us/icon/${address}` : 'https://via.placeholder.com/64')
-        .setFooter(user.username, user.displayAvatarURL({ dynamic: true }));
-
-      const targetChannelId = process.env.CHANNEL_ID;
-      const targetChannel = await interaction.client.channels.fetch(targetChannelId);
-      const sentMessage = await targetChannel.send({ embeds: [embed] });
-
-      console.log('Message sent successfully:', sentMessage);
-
-      // Update the user's last execution time
-      updateUserLastExecutionTime(userId);
-
-      await interaction.reply(`Server information sent to <#${targetChannelId}>. [View Message](${sentMessage.url})`);
-      console.log('Interaction reply successful');
-    } catch (error) {
-      console.error(`Error showcasing server for user ${userId}:`, error);
-      await interaction.reply('There was an error showcasing the server information.');
+    } catch (overallError) {
+      console.error('Overall Error:', overallError.message);
+      await interaction.reply('There was an unexpected error executing the command.');
     }
   },
 };
