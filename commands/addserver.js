ca93f3ce5069ccc93c5fc989ae9a057ea8870128
G9 Aerospace <80208426+g9militantsYT@users.noreply.github.com>
2023-12-18 11:19:44 +0530

    fixed the addserver command

--- commands/addserver.js
+++ commands/addserver.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('addserver')
-    .setDescription('Add server information')
+    .setDescription('Add or update server information')
     .addStringOption(option =>
       option.setName('address')
         .setDescription('Server Address (IP:Port or Domain)')
@@ -15,52 +15,59 @@ module.exports = {
         .setDescription('Custom Message')
         .setRequired(true)),
   async execute(interaction) {
-    const userId = interaction.user.id;
-    let address = interaction.options.getString('address');
-    
-    // Check if the address contains a port, if not, check if it looks like a SRV record
-    if (!address.includes(':')) {
-      const isSRVRecord = address.startsWith('_') && address.includes('.');
-      
-      if (!isSRVRecord) {
-        // Notify the user that they provided an address without a port and suggest it might be a SRV record
+    try {
+      const userId = interaction.user.id;
+      const address = interaction.options.getString('address');
+      const userMessage = interaction.options.getString('message');
+
+      const serverData = {
+        address,
+        message: userMessage,
+      };
+
+      const serversFolder = './servers';
+      const filePath = `${serversFolder}/${userId}.json`;
+
+      // Check if the file already exists
+      if (fs.existsSync(filePath)) {
+        // If the interaction has already been replied to, do nothing
+        if (interaction.replied || interaction.deferred) return;
+
+        // If the file exists, read the existing data
+        const existingData = JSON.parse(fs.readFileSync(filePath));
+
+        // Update the existing data with the new serverData
+        Object.assign(existingData, serverData);
+
+        // Write the updated data back to the file
+        fs.writeFileSync(filePath, JSON.stringify(existingData, null, 2));
+
+        // Notify the user that their data has been updated
         await interaction.reply({
-          content: 'You provided a server address without specifying a port. This may function improperly if it\'s not a SRV record. If it is a SRV record, you can ignore this message.',
+          content: 'Server information updated successfully!',
           ephemeral: true,
         });
-        return;
+      } else {
+        // If the interaction has already been replied to, do nothing
+        if (interaction.replied || interaction.deferred) return;
+
+        // If the file doesn't exist, create a new one with the provided data
+        fs.writeFileSync(filePath, JSON.stringify(serverData, null, 2));
+
+        // Notify the user that their data has been added
+        await interaction.reply({
+          content: 'Server information added successfully!',
+          ephemeral: true,
+        });
       }
 
-      // If it's a SRV record, append a default port (e.g., 80)
-      address += ':80';
-    }
+      // Log information to console
+      console.log(`Server information processed for user ${userId}.`);
+      console.log('Server Data:', serverData);
 
-    const serverData = {
-      address,
-      message: interaction.options.getString('message'),
-    };
-
-    try {
-      const serversFolder = './servers';
-      if (!fs.existsSync(serversFolder)) {
-        fs.mkdirSync(serversFolder);
-      }
-
-      const filePath = `${serversFolder}/${userId}.json`;
-      fs.writeFileSync(filePath, JSON.stringify(serverData, null, 2));
-
-      // Create a MessageEmbed with blue color
-      const embed = new MessageEmbed()
-        .setTitle('Server Information')
-        .setDescription('Server information added successfully!')
-        .setFooter('Embernodes', 'attachment://embernodes.png')
-        .setColor('BLUE'); // Set color to blue
-
-      // Send the embed along with the image
-      await interaction.reply({ embeds: [embed], files: ['./embernodes.png'] });
     } catch (error) {
-      console.error(`Error adding server information for user ${userId}: ${error}`);
-      await interaction.reply('There was an error while adding server information.');
+      console.error(`Error processing server information for user ${interaction.user.id}:`, error);
+      await interaction.reply('There was an error while processing server information.');
     }
   },
 };
